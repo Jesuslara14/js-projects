14e60cdb513f8a2dc4dbd7a9801f113747c34187
Jesuslara14 <111470200+Jesuslara14@users.noreply.github.com>
2024-03-14 14:28:18 -0700

    made wedensda porjedcts

---
+++ practice/script.js
@@ -0,0 +1,26 @@
+
+const a = () => {
+    let variable = '( ͡° ͜ʖ ͡°)'
+    for(let i = 0; i< variable.length; i++){
+        console.log(variable[i])
+    }
+    console.log(variable.length)
+    console.log(variable)
+}
+
+const b = () => {
+    let variable = document.getElementById('open').innerHTML
+    console.log(variable)
+}
+
+const c = () => {
+    console.log(`nice to meet ${g}`)
+
+    var g = 'you'
+}
+
+//a()
+
+//b()
+
+c()
